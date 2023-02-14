509e8f75a4e6d7191618e727a05cc0bb85d90034
Timo Wilde <timowilde2007@gmail.com>
2023-02-14 07:48:32 +0100

    removed create Page

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import {Route, Routes}  from 'react-router-dom'
 
 
 //components
-import CreatePage from './components/CreatePage/CreatePage.component'
 import Editor from './components/Editor/Editor.component'
 
 
@@ -14,13 +13,10 @@ function App() {
   window.onbeforeunload = function() {
     return "Data will be lost if you leave the page, are you sure?";
   };
-
+  
   return (
     <div className="App">
-      <Routes>
-        <Route path='/' element={<CreatePage />}/>
-        <Route path='/editor' element={<Editor />}/>
-      </Routes>
+      <Editor />
     </div>
   )
 }
